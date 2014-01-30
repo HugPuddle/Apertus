6aca6c9086abaa117f3d737bcae67741e60896be
embii <embii@embii.com>
2014-01-29 20:29:54 -0600

    Update RPCRequest.cs
    Give Credit

--- ADD/coinRPC/RPCRequest.cs
+++ ADD/coinRPC/RPCRequest.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 
 namespace ADD.RPCClient
 {
+	//Courtesy mb300sd Bitcoin.NET Implementation
 	[JsonObject(MemberSerialization=MemberSerialization.Fields)]
 	public class RPCRequest
 	{
